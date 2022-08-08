991353aa7bf3bee2c5a1532b0b6ad215e4cae793
Graham Bates <info@grahambates.com>
2022-08-08 20:57:37 +0100

    Initial import

---
+++ src/colorConvert.js
@@ -0,0 +1,181 @@
+export function rgb4ToRgb8(rgb4) {
+  return rgb4.map((c) => c * 17).map((v) => clamp(v, 0, 255));
+}
+
+export function rgb8ToRgb4(rgb8) {
+  return rgb8.map((c) => Math.floor(c / 16)).map((v) => clamp(v, 0, 15));
+}
+
+export function quantize4Bit(rgb8) {
+  return rgb4ToRgb8(rgb8ToRgb4(rgb8));
+}
+
+export function linearToSrgb(x) {
+  const y = x <= 0.0031308 ? 12.92 * x : 1.055 * x ** (1 / 2.4) - 0.055;
+  return Math.round(y * 255);
+}
+
+export function srgbToLinear(x) {
+  x /= 255.0;
+  return x <= 0.04045 ? x / 12.92 : ((x + 0.055) / 1.055) ** 2.4;
+}
+
+export function rgbToHsv([r, g, b]) {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  const max = Math.max(r, g, b);
+  const min = Math.min(r, g, b);
+  let h,
+    s,
+    v = max;
+
+  var d = max - min;
+  s = max === 0 ? 0 : d / max;
+
+  if (max === min) {
+    h = 0; // achromatic
+  } else {
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+      default:
+    }
+
+    h /= 6;
+  }
+
+  return [h, s, v];
+}
+
+export function hsvToRgb([h, s, v]) {
+  let r, g, b;
+
+  const i = Math.floor(h * 6);
+  const f = h * 6 - i;
+  const p = v * (1 - s);
+  const q = v * (1 - f * s);
+  const t = v * (1 - (1 - f) * s);
+
+  switch (i % 6) {
+    case 0:
+      r = v;
+      g = t;
+      b = p;
+      break;
+    case 1:
+      r = q;
+      g = v;
+      b = p;
+      break;
+    case 2:
+      r = p;
+      g = v;
+      b = t;
+      break;
+    case 3:
+      r = p;
+      g = q;
+      b = v;
+      break;
+    case 4:
+      r = t;
+      g = p;
+      b = v;
+      break;
+    case 5:
+      r = v;
+      g = p;
+      b = q;
+      break;
+    default:
+  }
+
+  return [r * 255, g * 255, b * 255];
+}
+
+export function labToRgb(lab) {
+  let y = (lab[0] + 16) / 116,
+    x = lab[1] / 500 + y,
+    z = y - lab[2] / 200,
+    r,
+    g,
+    b;
+
+  x = 0.95047 * (x * x * x > 0.008856 ? x * x * x : (x - 16 / 116) / 7.787);
+  y = 1.0 * (y * y * y > 0.008856 ? y * y * y : (y - 16 / 116) / 7.787);
+  z = 1.08883 * (z * z * z > 0.008856 ? z * z * z : (z - 16 / 116) / 7.787);
+
+  r = x * 3.2406 + y * -1.5372 + z * -0.4986;
+  g = x * -0.9689 + y * 1.8758 + z * 0.0415;
+  b = x * 0.0557 + y * -0.204 + z * 1.057;
+
+  r = r > 0.0031308 ? 1.055 * Math.pow(r, 1 / 2.4) - 0.055 : 12.92 * r;
+  g = g > 0.0031308 ? 1.055 * Math.pow(g, 1 / 2.4) - 0.055 : 12.92 * g;
+  b = b > 0.0031308 ? 1.055 * Math.pow(b, 1 / 2.4) - 0.055 : 12.92 * b;
+
+  return [
+    Math.max(0, Math.min(1, r)) * 255,
+    Math.max(0, Math.min(1, g)) * 255,
+    Math.max(0, Math.min(1, b)) * 255,
+  ];
+}
+
+export function rgbToLab(rgb) {
+  let r = rgb[0] / 255,
+    g = rgb[1] / 255,
+    b = rgb[2] / 255,
+    x,
+    y,
+    z;
+
+  r = r > 0.04045 ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+  g = g > 0.04045 ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+  b = b > 0.04045 ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+
+  x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+  y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.0;
+  z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+
+  x = x > 0.008856 ? Math.pow(x, 1 / 3) : 7.787 * x + 16 / 116;
+  y = y > 0.008856 ? Math.pow(y, 1 / 3) : 7.787 * y + 16 / 116;
+  z = z > 0.008856 ? Math.pow(z, 1 / 3) : 7.787 * z + 16 / 116;
+
+  return [116 * y - 16, 500 * (x - y), 200 * (y - z)];
+}
+
+export function luminance([r, g, b]) {
+  return 0.299 * r + 0.587 * g + 0.114 * b;
+}
+
+export function rgbCssProp([r, g, b]) {
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+export function fToPercent(val) {
+  return Math.round(val * 100) + "%";
+}
+
+export function rgb4ToHex(rgb) {
+  return rgb.map((v) => v.toString(16)).join("");
+}
+
+export function hexToRgb4(hex) {
+  return hex.split("").map((v) => parseInt(v, 16));
+}
+
+export function clamp(value, min = 0, max = 1) {
+  return Math.min(Math.max(value, min), max);
+}
+
+export function sameColors(a, b) {
+  return a.join(",") === b.join(",");
+}
---
+++ src/gradient.js
@@ -0,0 +1,222 @@
+import * as conv from "./colorConvert";
+
+const GOLDEN_RATIO = 1.61803399;
+
+export function buildGradient(
+  points,
+  steps,
+  blendMode,
+  ditherMode = "off",
+  ditherAmount = 0
+) {
+  const mappedPoints = [...points].map((p) => ({
+    color: conv.quantize4Bit(conv.hsvToRgb(p.color)),
+    y: Math.round(p.pos * (steps - 1)),
+  }));
+
+  const values = [];
+  let pointIndex = 0;
+
+  for (let i = 0; i < steps; i++) {
+    const current = mappedPoints[pointIndex];
+    const next = mappedPoints[pointIndex + 1];
+
+    const firstPoint = current.y >= i;
+    const lastPoint = !next;
+
+    if (firstPoint || lastPoint) {
+      // Use exact color value
+      values.push(current.color);
+    } else if (next.y === i) {
+      // Reached next point
+      pointIndex++;
+      values.push(next.color);
+    } else {
+      // Mix intermediate step:
+      const pos = (i - current.y) / (next.y - current.y);
+      const from = current.color;
+      const to = next.color;
+      let mixed;
+      switch (blendMode) {
+        case "lab":
+          mixed = conv.labToRgb(
+            lerpTuple(conv.rgbToLab(from), conv.rgbToLab(to), pos)
+          );
+          break;
+        case "perceptual":
+          mixed = perceptualMix(from, to, pos);
+          break;
+        default:
+          mixed = lerpTuple(from, to, pos);
+      }
+      values.push(mixed);
+    }
+  }
+
+  if (ditherMode !== "off") {
+    dither(values, ditherMode, ditherAmount);
+  }
+
+  return values.map(conv.rgb8ToRgb4);
+}
+
+// https://stackoverflow.com/questions/22607043/color-gradient-algorithm
+function perceptualMix(color1, color2, pos) {
+  const from = color1.map(conv.srgbToLinear);
+  const to = color2.map(conv.srgbToLinear);
+  const mixed = [
+    from[0] + (to[0] - from[0]) * pos,
+    from[1] + (to[1] - from[1]) * pos,
+    from[2] + (to[2] - from[2]) * pos,
+  ];
+
+  // Compute a measure of brightness of the two colors using empirically determined gamma
+  const gamma = 0.43;
+  const fromBrightness = Math.pow(from[0] + from[1] + from[2], gamma);
+  const toBrightness = Math.pow(to[0] + to[1] + to[2], gamma);
+
+  // Interpolate a new brightness value, and convert back to linear light
+  const brightness = fromBrightness + (toBrightness - fromBrightness) * pos;
+  const intensity = Math.pow(brightness, 1 / gamma);
+
+  // Apply adjustment factor to each rgb value based
+  const sum = mixed[0] + mixed[1] + mixed[2];
+  if (sum > 0) {
+    const factor = intensity / sum;
+    mixed[0] *= factor;
+    mixed[1] *= factor;
+    mixed[2] *= factor;
+  }
+
+  return mixed.map(conv.linearToSrgb);
+}
+
+function dither(values, ditherMode, ditherAmount) {
+  let swappedLast = false;
+
+  for (let i = 0; i < values.length; i++) {
+    const amount = ditherAmount / 100;
+    switch (ditherMode) {
+      case "shuffle": {
+        if (i > 0) {
+          const prev = values[i - 1];
+          if (
+            !swappedLast &&
+            !conv.sameColors(conv.rgb8ToRgb4(prev), conv.rgb8ToRgb4(values[i]))
+          ) {
+            values[i - 1] = values[i];
+            values[i] = prev;
+            swappedLast = true;
+          } else {
+            swappedLast = false;
+          }
+        }
+        break;
+      }
+      case "errorDiffusion": {
+        let errR = err4bit(values[i][0]);
+        let errG = err4bit(values[i][1]);
+        let errB = err4bit(values[i][2]);
+        if (values[i + 1]) {
+          values[i + 1][0] += errR * amount;
+          values[i + 1][1] += errG * amount;
+          values[i + 1][2] += errB * amount;
+        }
+        break;
+      }
+      case "ordered":
+        values[i][0] += ((bayer16[i % 16] - 128) / 17) * amount;
+        values[i][1] += ((bayer16[(i % 16) + 16] - 128) / 17) * amount;
+        values[i][2] += ((bayer16[(i % 16) + 32] - 128) / 17) * amount;
+        break;
+      case "orderedMono": {
+        const offset = (i % 2 ? 4 : -4) * amount;
+        values[i][0] += offset;
+        values[i][1] += offset;
+        values[i][2] += offset;
+        break;
+      }
+      case "blueNoise": {
+        values[i][0] += blueNoise[i % 64] * 17 * amount;
+        values[i][1] += blueNoise[(i + 16) % 64] * 17 * amount;
+        values[i][2] += blueNoise[(i + 32) % 64] * 17 * amount;
+        break;
+      }
+      case "blueNoiseMono": {
+        const ofs = blueNoise[i % 64] * 17 * amount;
+        values[i][0] += ofs;
+        values[i][1] += ofs;
+        values[i][2] += ofs;
+        break;
+      }
+      case "whiteNoise":
+        values[i][0] += (Math.random() * 17 - 8.5) * amount;
+        values[i][1] += (Math.random() * 17 - 8.5) * amount;
+        values[i][2] += (Math.random() * 17 - 8.5) * amount;
+        break;
+      case "whiteNoiseMono":
+        const ofs = (Math.random() - 0.5) * 17 * amount;
+        values[i][0] += ofs;
+        values[i][1] += ofs;
+        values[i][2] += ofs;
+        break;
+      case "goldenRatio": {
+        values[i][0] += (((i * GOLDEN_RATIO) % 1) - 0.5) * 17 * amount;
+        values[i][1] += ((((i + 1) * GOLDEN_RATIO) % 1) - 0.5) * 17 * amount;
+        values[i][2] += ((((i + 3) * GOLDEN_RATIO) % 1) - 0.5) * 17 * amount;
+        break;
+      }
+      case "goldenRatioMono": {
+        const ofs = (((i * GOLDEN_RATIO) % 1) - 0.5) * 17 * amount;
+        values[i][0] += ofs;
+        values[i][1] += ofs;
+        values[i][2] += ofs;
+        break;
+      }
+      default:
+    }
+  }
+}
+
+function err4bit(val) {
+  let err = val % 17;
+  if (err > 8.5) {
+    err -= 8.5;
+  }
+  return err;
+}
+
+function lerpTuple(from, to, pos) {
+  const ret = [
+    Math.round(from[0] + (to[0] - from[0]) * pos),
+    Math.round(from[1] + (to[1] - from[1]) * pos),
+    Math.round(from[2] + (to[2] - from[2]) * pos),
+  ];
+  return ret;
+}
+
+const blueNoise = [
+  18, 59, 10, 35, 49, 22, 6, 53, 27, 41, 13, 63, 20, 37, 1, 48, 25, 57, 9, 34,
+  44, 16, 51, 4, 31, 62, 19, 39, 11, 47, 23, 56, 0, 32, 45, 14, 60, 28, 7, 50,
+  38, 15, 29, 54, 2, 42, 24, 61, 12, 36, 21, 52, 5, 40, 26, 58, 8, 33, 46, 17,
+  55, 3, 30, 43,
+].map((n) => n / 64 - 0.5);
+
+const bayer16 = [
+  0, 191, 48, 239, 12, 203, 60, 251, 3, 194, 51, 242, 15, 206, 63, 254, 127, 64,
+  175, 112, 139, 76, 187, 124, 130, 67, 178, 115, 142, 79, 190, 127, 32, 223,
+  16, 207, 44, 235, 28, 219, 35, 226, 19, 210, 47, 238, 31, 222, 159, 96, 143,
+  80, 171, 108, 155, 92, 162, 99, 146, 83, 174, 111, 158, 95, 8, 199, 56, 247,
+  4, 195, 52, 243, 11, 202, 59, 250, 7, 198, 55, 246, 135, 72, 183, 120, 131,
+  68, 179, 116, 138, 75, 186, 123, 134, 71, 182, 119, 40, 231, 24, 215, 36, 227,
+  20, 211, 43, 234, 27, 218, 39, 230, 23, 214, 167, 104, 151, 88, 163, 100, 147,
+  84, 170, 107, 154, 91, 166, 103, 150, 87, 2, 193, 50, 241, 14, 205, 62, 253,
+  1, 192, 49, 240, 13, 204, 61, 252, 129, 66, 177, 114, 141, 78, 189, 126, 128,
+  65, 176, 113, 140, 77, 188, 125, 34, 225, 18, 209, 46, 237, 30, 221, 33, 224,
+  17, 208, 45, 236, 29, 220, 161, 98, 145, 82, 173, 110, 157, 94, 160, 97, 144,
+  81, 172, 109, 156, 93, 10, 201, 58, 249, 6, 197, 54, 245, 9, 200, 57, 248, 5,
+  196, 53, 244, 137, 74, 185, 122, 133, 70, 181, 118, 136, 73, 184, 121, 132,
+  69, 180, 117, 42, 233, 26, 217, 38, 229, 22, 213, 41, 232, 25, 216, 37, 228,
+  21, 212, 169, 106, 153, 90, 165, 102, 149, 86, 168, 105, 152, 89, 164, 101,
+  148, 85,
+];
