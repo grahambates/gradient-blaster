461db48362dd810d1dd55aa0148fbe2e935f96dc
Graham Bates <info@grahambates.com>
2022-08-11 09:57:11 +0100

    Layout change in left column

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import * as conv from "../lib/colorConvert";
 
 import "./Detail.css";
 import {
@@ -30,23 +31,58 @@ function Detail() {
     dispatch(setPos(pos));
   };
 
+  const [hex, setHex] = useState();
+
+  const setRgb4 = newRgb4 => {
+    dispatch(setColor(conv.rgbToHsv(conv.rgb4ToRgb8(newRgb4))));
+  };
+
+  useEffect(() => {
+    const rgb4 = conv.rgb8ToRgb4(conv.hsvToRgb(selectedPoint.color));
+    setHex(conv.rgb4ToHex(rgb4));
+  }, [selectedPoint.color]);
+
   return (
     <div className="Detail">
+      <div className="Detail__nav">
+        <button
+          onClick={() => dispatch(previousPoint())}
+          disabled={!selectedIndex}
+        >
+          &lt;
+        </button>{" "}
+        {selectedIndex + 1}/{points.length}{" "}
+        <button
+          onClick={() => dispatch(nextPoint())}
+          disabled={selectedIndex >= points.length - 1}
+        >
+          &gt;
+        </button>
+      </div>
       <div className="Detail__info">
-        <div className="Detail__nav">
-          <button
-            onClick={() => dispatch(previousPoint())}
-            disabled={!selectedIndex}
-          >
-            &lt;
-          </button>{" "}
-          {selectedIndex + 1}/{points.length}{" "}
-          <button
-            onClick={() => dispatch(nextPoint())}
-            disabled={selectedIndex >= points.length - 1}
-          >
-            &gt;
-          </button>
+        <div className="Detail__color">
+          <div
+            className="Detail__swatch"
+            style={{
+              background: conv.rgbCssProp(
+                conv.quantize4Bit(conv.hsvToRgb(selectedPoint.color))
+              )
+            }}
+          />
+          <label htmlFor="Detail-hex">#</label>
+          <input
+            id="Detail-hex"
+            className="Detail__hexInput"
+            value={hex}
+            onChange={e => {
+              const newHex = e.target.value;
+              setHex(newHex);
+              if (newHex.match(/^[0-9a-f]{3}$/i)) {
+                const newRgb4 = conv.hexToRgb4(newHex);
+                setRgb4(newRgb4);
+              }
+            }}
+          />
         </div>
         <div className="Detail__position">
           <label htmlFor="position">Pos: </label>
