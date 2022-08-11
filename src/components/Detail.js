f72a0702e2586890716b9e3259a1e527eb3a71c4
Graham Bates <info@grahambates.com>
2022-08-11 11:50:35 +0100

    UI improvements

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -42,58 +42,31 @@ function Detail() {
     setHex(conv.rgb4ToHex(rgb4));
   }, [selectedPoint.color]);
 
+  const rgb = conv.hsvToRgb(selectedPoint.color);
+  const color = conv.rgbCssProp(conv.quantize4Bit(rgb));
+  const light = conv.luminance(rgb) > 128;
+
   return (
-    <div className="Detail">
-      <div className="Detail__nav">
-        <button
-          onClick={() => dispatch(previousPoint())}
-          disabled={!selectedIndex}
-        >
-          &lt;
-        </button>{" "}
-        {selectedIndex + 1}/{points.length}{" "}
-        <button
-          onClick={() => dispatch(nextPoint())}
-          disabled={selectedIndex >= points.length - 1}
-        >
-          &gt;
-        </button>
-      </div>
-      <div className="Detail__info">
-        <div className="Detail__color">
-          <div
-            className="Detail__swatch"
-            style={{
-              background: conv.rgbCssProp(
-                conv.quantize4Bit(conv.hsvToRgb(selectedPoint.color))
-              )
-            }}
-          />
-          <label htmlFor="Detail-hex">#</label>
-          <input
-            id="Detail-hex"
-            className="Detail__hexInput"
-            value={hex}
-            onChange={e => {
-              const newHex = e.target.value;
-              setHex(newHex);
-              if (newHex.match(/^[0-9a-f]{3}$/i)) {
-                const newRgb4 = conv.hexToRgb4(newHex);
-                setRgb4(newRgb4);
-              }
-            }}
-          />
-        </div>
-        <div className="Detail__position">
-          <label htmlFor="position">Pos: </label>
-          <input
-            id="position"
-            type="number"
-            min={0}
-            max={steps - 1}
-            value={Math.round(selectedPoint.pos * (steps - 1))}
-            onChange={e => handleMove(e.target.value)}
-          />
+    <section className="Detail">
+      <header
+        className="Detail__header"
+        style={{ background: color, color: light ? "inherit" : "white" }}
+      >
+        <div className="Detail__nav">
+          Point:{" "}
+          <button
+            onClick={() => dispatch(previousPoint())}
+            disabled={!selectedIndex}
+          >
+            &lt;
+          </button>{" "}
+          {selectedIndex + 1}/{points.length}{" "}
+          <button
+            onClick={() => dispatch(nextPoint())}
+            disabled={selectedIndex >= points.length - 1}
+          >
+            &gt;
+          </button>
         </div>
         <button
           type="button"
@@ -102,12 +75,43 @@ function Detail() {
         >
           Remove
         </button>
+      </header>
+      <div className="Detail__body">
+        <div className="Detail__info">
+          <div className="Detail__color">
+            <label htmlFor="Detail-hex">Color: $</label>
+            <input
+              id="Detail-hex"
+              className="Detail__hexInput"
+              value={hex}
+              onChange={e => {
+                const newHex = e.target.value;
+                setHex(newHex);
+                if (newHex.match(/^[0-9a-f]{3}$/i)) {
+                  const newRgb4 = conv.hexToRgb4(newHex);
+                  setRgb4(newRgb4);
+                }
+              }}
+            />
+          </div>
+          <div className="Detail__position">
+            <label htmlFor="position">Position: </label>
+            <input
+              id="position"
+              type="number"
+              min={0}
+              max={steps - 1}
+              value={Math.round(selectedPoint.pos * (steps - 1))}
+              onChange={e => handleMove(e.target.value)}
+            />
+          </div>
+        </div>
+        <Picker
+          hsv={selectedPoint.color}
+          onChange={color => dispatch(setColor(color))}
+        />
       </div>
-      <Picker
-        hsv={selectedPoint.color}
-        onChange={color => dispatch(setColor(color))}
-      />
-    </div>
+    </section>
   );
 }
 
