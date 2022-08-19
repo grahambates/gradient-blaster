ca40d67278873a42c9474d4077ed30a89d7e1625
Graham Bates <info@grahambates.com>
2022-08-19 11:10:41 +0100

    Add Pouet link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 import { FaGithub } from "react-icons/fa";
+import { GiTrumpet } from "react-icons/gi";
 
 function Footer() {
   return (
@@ -14,6 +15,10 @@ function Footer() {
         <a href="https://github.com/grahambates/gradient-blaster">
           Source on Github
         </a>
+      </div>
+      <div>
+        <GiTrumpet />
+        <a href="https://www.pouet.net/prod.php?which=92033">Pouet</a>
       </div>
     </div>
   );
